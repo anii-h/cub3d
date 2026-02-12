92c31cb8ec5379bcea398962456bfcfebc4ac5f5
anonym <annyann842@gmail.com>
2026-02-12 23:10:45 +0400

    update main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: anihamba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 17:54:20 by miyolchy          #+#    #+#             */
-/*   Updated: 2026/02/06 15:16:42 by anihamba         ###   ########.fr       */
+/*   Updated: 2026/02/12 22:38:37 by anihamba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,10 @@ int	main(void)
 	
 	init_mlx(&game, &map);
 
+	/* for now */
+	mlx_hook(game->mlx.win, 17, 0, on_window_close, &game);		// TODO - destroyNOtify - X
+	mlx_hook(game->mlx.win, 2, 1L<<0, on_key_press, &game);     // TODO - whne the key is pressed
+	mlx_hook(game->mlx.win, 3, 1L<<1, on_key_release, &game);	// TODO - when the key is released 
+	mlx_loop_hook(game->mlx.mlx, game_loop, &game);				// TODO - the main game loop
+	mlx_loop(game->mlx.mlx);
 }
