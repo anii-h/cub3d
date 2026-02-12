b882d411a214553092e548f8cf4951b07a840e07
anonym <annyann842@gmail.com>
2026-02-12 22:48:20 +0400

    update free_map

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: anihamba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 19:15:55 by anihamba          #+#    #+#             */
-/*   Updated: 2026/02/06 15:22:53 by anihamba         ###   ########.fr       */
+/*   Updated: 2026/02/12 22:47:23 by anihamba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,29 @@ void	free_map(t_map *map)
 	int	i;
 
 	i = 0;
+	if (!map)
+		return ;
 	if(map->map)
 	{
 		while (i < map->height)
 		{
-			free(map->map[i]);
+			if (map->map[i])
+				free(map->map[i]);
 			i++;
 		}	
 		free(map->map);
 		map->map = NULL;
 	}
+	t_mapnode	*cur = map->tmp_head;
+	while(cur)
+	{
+		if (cur->line)
+			free(cur->line);
+		t_mapnode *temp = cur->next;
+		free(cur);
+		cur = temp;
+	}
+	map->tmp_head = NULL;
 	map->height = 0;
 	map->width = 0;
 }
